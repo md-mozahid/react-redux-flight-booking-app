0d1fb6f61c64fbda5283e8961aa8980691884e88
mozahid <ce.muzahid@gmail.com>
2023-05-23 21:50:01 +0600

    add redux dev tools extension

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
-import FlightReducer from "./FlightReducer";
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+import FlightReducer from './FlightReducer'
 
-const store = createStore(FlightReducer);
+const store = createStore(
+  FlightReducer,
+  composeWithDevTools(applyMiddleware(logger))
+)
 
-export default store;
+export default store
